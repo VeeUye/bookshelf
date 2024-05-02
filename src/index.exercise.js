7641cf91c7fa4a7a1ff6e2e48d3d84177fc2769a
Vee Uye <vee.uye@gmail.com>
2024-05-02 12:06:17 +0100

    feat: use  @reach/dialog to create login and register modals

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,19 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {createRoot} from 'react-dom/client'
+import {Dialog} from '@reach/dialog'
 
 import {Logo} from './components/logo'
 
-const handleOnClick = () => {
-  console.log('button clicked')
-}
+import '@reach/dialog/styles.css'
+import {VisuallyHidden} from '@reach/visually-hidden'
 
 const App = () => {
+  const [openModal, setOpenModal] = useState('none')
+
   return (
     <>
       <h1>Bookshelf</h1>
       <Logo />
-      <button onClick={handleOnClick}>Login</button>
-      <button onClick={handleOnClick}>Register</button>
+      <button onClick={() => setOpenModal('login')}>Login</button>
+      <button onClick={() => setOpenModal('register')}>Register</button>
+
+      <Dialog aria-label="login-form" isOpen={openModal === 'login'}>
+        <button onClick={() => setOpenModal('none')}>
+          <VisuallyHidden>Close</VisuallyHidden>
+          <span aria-hidden>x</span>
+        </button>
+        <p>Login modal</p>
+      </Dialog>
+      <Dialog aria-label="register-form" isOpen={openModal === 'register'}>
+        <button onClick={() => setOpenModal('none')}>
+          <VisuallyHidden>Close</VisuallyHidden>
+          <span aria-hidden>x</span>
+        </button>
+        <p>Register modal</p>
+      </Dialog>
     </>
   )
 }
