ed3af879dc678273195593e5d8cd56b01bc808f5
VeeUye <97988558+VeeUye@users.noreply.github.com>
2024-05-02 11:21:19 +0100

    Initialize exercises/04-authentication

---
+++ src/utils/api-client.js
@@ -0,0 +1,9 @@
+// no final
+
+export * from './api-client.exercise'
+
+// 💯 automatically logout on 401
+// export * from './api-client.extra-3'
+
+// 💯 Support posting data
+// export * from './api-client.extra-4'
