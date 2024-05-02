41fb51968e8e65c6b8e146ebd88fdb2b502e0bc0
VeeUye <97988558+VeeUye@users.noreply.github.com>
2024-05-02 11:21:08 +0100

    Initialize old/11-unit-testing

---
+++ src/setupTests.js
@@ -0,0 +1,6 @@
+export * from './setupTests.final'
+
+// export * from './setupTests.exercise'
+
+// 💯 Test failure cases
+// export * from './setupTests.extra-2'
---
+++ src/utils/__tests__/api-client.js
@@ -0,0 +1,12 @@
+export * from './api-client.final'
+
+// export * from './api-client.exercise'
+
+// 💯 Stop mocking fetch
+// export * from './api-client.extra-1'
+
+// 💯 Test failure cases
+// export * from './api-client.extra-2'
+
+// 💯 Use `setupTests.js`
+// export * from './api-client.extra-3'
